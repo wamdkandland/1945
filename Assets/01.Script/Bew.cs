faac3db6e48fa5aad8e77973802160845318753b
김태호 <uji4568@naver.com>
2025-03-17 16:40:38 +0900

    250317

---
+++ Assets/01.Script/Bew.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bew : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 1080x1920 해상도로 설정 (창 모드: false → 전체 화면)
+        Screen.SetResolution(1080, 1920, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
