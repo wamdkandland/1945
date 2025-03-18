04ad115d76987a761b38cb301d3b3a735d8ad943
김태호 <uji4568@naver.com>
2025-03-18 13:32:18 +0900

    0318

--- Assets/01.Script/Bew.cs
+++ Assets/01.Script/Bew.cs
@@ -6,7 +6,7 @@ public class Bew : MonoBehaviour
     void Start()
     {
         // 1080x1920 해상도로 설정 (창 모드: false → 전체 화면)
-        Screen.SetResolution(1080, 1920, true);
+        Screen.SetResolution(1080, 1920, false);
     }
 
     // Update is called once per frame
